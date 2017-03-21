7120a42855e07a6de361fc83205677dbdd100082
Jason Filippou <jason.filippou@gmail.com>
2017-03-21 00:59:35 -0400

    First commit. One interface, one implementing class. Non-compilable state.
    TODOS:
    
    (1) Switch from implementing classes to defining their API and authoring the testcases. The testcases need to be common. Only thing you should be changing is the object pointed to by the Bag reference.
    
    (2)  Create a client program that will time the access of the elements after shaking the bag. Goal: estimate impact of partial cache storage of large arrays.

---
+++ src/edu/umd/cs/datastructures/bags/StaticallyPerturbedBag.java
@@ -0,0 +1,61 @@
+package edu.umd.cs.datastructures.bags;
+
+import java.util.Iterator;
+
+/**
+ *
+ * A StaticallyPerturbedBag is a bag where shaking perturbs the elements in a pre-defined way. For this example, the way
+ * will be (index + offset) MOD length.
+ * @implNote The class uses a static array for storing of <b>Item</b>s.
+ * @author jason
+ * @version 1.0
+ */
+public class StaticallyPerturbedBag<Item> implements Bag{
+
+
+    private int current;
+    private Item[] storage;
+    private static int DEFAULT_CAPACITY = 10;
+    private static int OFFSET=3;
+
+    public StaticallyPerturbedBag(){
+        storage = (Item[])new Object[DEFAULT_CAPACITY]; //TODO: See if this will actually work
+        current =0;
+    }
+
+    public StaticallyPerturbedBag(int initCapacity){
+        storage = (Item[])new Object[initCapacity];
+        current = 0;
+    }
+
+    // Let's not spend time on equals(), extended hashcode(), copy constructors...
+
+    public void add(Object i) {
+
+    }
+
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+
+    public void shake() {
+
+    }
+
+
+    public Iterator iterator() {
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Object next() {
+                return null;
+            }
+        }
+    }
+}
