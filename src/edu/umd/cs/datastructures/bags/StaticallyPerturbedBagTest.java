ea7c7632d2c4bf3ad8aa18f99cbabd629f536d39
Jason Filippou <jason.filippou@gmail.com>
2017-03-22 20:09:34 -0400

    First commit in Mac where I was able to add IntelliJ's jUnit4.

---
+++ src/edu/umd/cs/datastructures/bags/StaticallyPerturbedBagTest.java
@@ -0,0 +1,23 @@
+package edu.umd.cs.datastructures.bags;
+
+/**
+ * Created by jason on 3/22/17.
+ */
+public class StaticallyPerturbedBagTest {
+    @org.junit.Test
+    public void add() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isEmpty() throws Exception {
+    }
+
+    @org.junit.Test
+    public void shake() throws Exception {
+    }
+
+    @org.junit.Test
+    public void iterator() throws Exception {
+    }
+
+}
