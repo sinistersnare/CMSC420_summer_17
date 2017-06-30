3e7ff80738ba84be53130018b86e3f86a8863cc3
Jason Filippou <jason.filippou@gmail.com>
2017-06-29 23:47:23 -0400

    Huffman trie progress at Matt's place, Java 8 features and whatnot.

--- src/edu/umd/cs/datastructures/demos/strings/HuffmanEncoder.java
+++ src/edu/umd/cs/datastructures/demos/strings/HuffmanEncoder.java
@@ -11,24 +11,7 @@ public class HuffmanEncoder {
 
     private String input;
 
-    private class Node {
-        Character c; // Only non-null for leaf nodes. 
-        long freq;
-        Node left, right;
-
-        Node(Character c){
-            this.c = c;
-        }
-
-        long getFreq(){
-            return freq;
-        }
-
-        Character getC(){
-            return c;
-        }
-    }
-
+    private HuffmanTrieNode root;
 
     public HuffmanEncoder(){
         input = "";
@@ -43,7 +26,7 @@ public class HuffmanEncoder {
     }
 
     public void encode(String s){
-        List<Node> chars = getCharsAndFreqs(s);
+        List<HuffmanTrieNode> nodes = getCharsAndFreqs(s);
 
         /* Huffman works best if you sort the nodes in ascending order of frequency.
          * This is because  heapify()  will run fastest when this occurs. Details:
@@ -52,21 +35,10 @@ public class HuffmanEncoder {
          *  about Huffman building the tree bottom-up is done in time linear to the input characters.
         */
 
-        chars.sort(Comparator.comparing(Node::getFreq).thenComparing(Node::getC)); // TODO: Test this in-place sorting
+        nodes.sort(Comparator.comparing(HuffmanTrieNode::getFreq).thenComparing(HuffmanTrieNode::getC)); // TODO: Test this in-place sorting
 
-        /*PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                if(n1.getFreq() < n2.getFreq())
-                    return -1;
-                else if(n1.getFreq() > n2.getFreq())
-                    return 1;
-                else
-                    return n1.getC().compareTo(n2.getC());
 
-            }
-        });*/
-        PriorityQueue<Node> pq = new PriorityQueue<Node>((n1, n2)-> {
+        PriorityQueue<HuffmanTrieNode> pq = new PriorityQueue<>((n1, n2)-> {
             if(n1.getFreq() < n2.getFreq())
                 return -1;
             else if(n1.getFreq() > n2.getFreq())
@@ -75,15 +47,19 @@ public class HuffmanEncoder {
                 return n1.getC().compareTo(n2.getC());
         });
 
-        chars.forEach(insert); // Unless otherwise specified by the implementor, actions are performed in the order of iteration
+        pq.addAll(nodes);
 
+        while(pq.size() > 1){
+            HuffmanTrieNode first = pq.remove(), second = pq.remove();
+            pq.add(new HuffmanTrieNode(first.getFreq() + second.getFreq(), first, second));
+        }
 
+        root = pq.remove();
 
     }
 
-    private List<Node> getCharsAndFreqs(String s){
-        return null;
-        //return s.chars().mapToObj(i -> new Node((char)i));
+    private List<HuffmanTrieNode> getCharsAndFreqs(String s){
+        return s.chars().forEach(c -> new HuffmanTrieNode((char)c));
     }
 
 }
